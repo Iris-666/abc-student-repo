00928982f371452d35a1e008f51814bbc2b699bc
Iris-666 <apple@10-209-63-59.dynapool.shanghai.nyu.edu>
2020-09-21 16:31:56 +0800

    update hyperlink gallery room

--- line-Iris/script.js
+++ line-Iris/script.js
@@ -1,14 +1,15 @@
-let welcome = document.getElementById('welcome')
+// let welcome = document.getElementById('welcome')
 let terminal = document.getElementById('terminal')
 let welcomeLeft = 1680
 let terminalLeft = 1680
-window.onload = function(){
-    let interval = setInterval(() => {
-        console.log(welcomeLeft)
-        welcomeLeft -= 2
-        welcome.style.left = welcomeLeft + 'px'
-        if(welcomeLeft < -400){
-           clearInterval(interval)
-        }
-    }, 10);
-}
+// window.onload = function(){
+//     let interval = setInterval(() => {
+//         // console.log(welcomeLeft)
+//         welcomeLeft -= 2
+//         welcome.style.left = welcomeLeft + 'px'
+//         if(welcomeLeft < -400){
+//            clearInterval(interval)
+//         }
+//     }, 10);
+// }
+
