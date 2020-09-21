19e05c9b890f858761fc8e9a26b7fa9cb5cc0d52
Iris-666 <apple@MacBook-Pro-2.local>
2020-09-21 11:09:17 +0800

    Folder update

---
+++ line-Iris/script.js
@@ -0,0 +1,14 @@
+let welcome = document.getElementById('welcome')
+let terminal = document.getElementById('terminal')
+let welcomeLeft = 1680
+let terminalLeft = 1680
+window.onload = function(){
+    let interval = setInterval(() => {
+        console.log(welcomeLeft)
+        welcomeLeft -= 2
+        welcome.style.left = welcomeLeft + 'px'
+        if(welcomeLeft < -400){
+           clearInterval(interval)
+        }
+    }, 10);
+}
