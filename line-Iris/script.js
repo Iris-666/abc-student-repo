c875af7f44c05f779d17114b8d8f3065e3cc85c3
Iris-666 <apple@tts-MacBook-Pro.local>
2020-11-26 13:45:24 +0800

    hyperlink gallery update

--- line-Iris/script.js
+++ line-Iris/script.js
@@ -1,15 +1,24 @@
-// let welcome = document.getElementById('welcome')
-let terminal = document.getElementById('terminal')
-let welcomeLeft = 1680
-let terminalLeft = 1680
-// window.onload = function(){
-//     let interval = setInterval(() => {
-//         // console.log(welcomeLeft)
-//         welcomeLeft -= 2
-//         welcome.style.left = welcomeLeft + 'px'
-//         if(welcomeLeft < -400){
-//            clearInterval(interval)
-//         }
-//     }, 10);
-// }
+let mini1 = document.getElementById("mini1")
+let mini2 = document.getElementById("mini2")
+let mini3 = document.getElementById("mini3")
+let mini4 = document.getElementById("mini4")
+let mini5 = document.getElementById("mini5")
+let mini6 = document.getElementById("mini6")
+let mini7 = document.getElementById("mini7")
+let projectA = document.getElementById("projectA")
+let projectB = document.getElementById("projectB")
+let projectC = document.getElementById("projectC")
+let links = [mini1, mini2, mini3, mini4, mini5, mini6, mini7, projectA, projectB, projectC]
 
+let scrollingText = document.getElementById("scrollingText")
+
+links.forEach(function(element) {
+        element.addEventListener("click", () => {
+            scrollingText.innerHTML = `Welcome to the metro line Iris &nbsp;&nbsp;&nbsp;&nbsp; The terminal station is Project C &nbsp;&nbsp;&nbsp;&nbsp; Next stop is ${element.id} &nbsp;&nbsp;&nbsp;&nbsp; please get ready to Exit, from the left side.`
+            console.log(element.id)
+        })
+    })
+    // mini1.addEventListener("click", () => {
+    //     console.log(scrollingText.innerHTML)
+    //     scrollingText.innerHTML = `Welcome to the metro line Iris &nbsp;&nbsp;&nbsp;&nbsp; The terminal station is Project C &nbsp;&nbsp;&nbsp;&nbsp; Next stop is Mini Project #6 &nbsp;&nbsp;&nbsp;&nbsp; please get ready to Exit, from the left side.`
+    // })
