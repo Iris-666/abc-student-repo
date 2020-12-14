a161895d8c90d8efc891ed6e6150ac422bc3703c
Iris-666 <apple@tts-MacBook-Pro.local>
2020-12-14 20:20:53 +0800

    hyperlink gallery update

--- line-Iris/script.js
+++ line-Iris/script.js
@@ -10,15 +10,53 @@ let projectB = document.getElementById("projectB")
 let projectC = document.getElementById("projectC")
 let links = [mini1, mini2, mini3, mini4, mini5, mini6, mini7, projectA, projectB, projectC]
 
-let scrollingText = document.getElementById("scrollingText")
+let door1 = document.getElementById("metroDoor");
+let door2 = document.getElementById("metroDoor2");
+let metroBackground = document.getElementById("metroBackground");
+
+let scrollingText = document.getElementById("scrollingText");
+
+door1.style.height = `${metroBackground.height * 0.84}px`
+door2.style.height = `${metroBackground.height * 0.84}px`
+door1.style.top = `${metroBackground.height * 0.17}px`
+door2.style.top = `${metroBackground.height * 0.17}px`
+
 
 links.forEach(function(element) {
-        element.addEventListener("click", () => {
-            scrollingText.innerHTML = `Welcome to the metro line Iris &nbsp;&nbsp;&nbsp;&nbsp; The terminal station is On The Moon &nbsp;&nbsp;&nbsp;&nbsp; Next stop is ${element.innerHTML} &nbsp;&nbsp;&nbsp;&nbsp; please get ready to Exit, from the left side.`
-            console.log(element.id)
-        })
+    element.addEventListener("click", () => {
+        scrollingText.innerHTML = `Welcome to the metro line Iris &nbsp;&nbsp;&nbsp;&nbsp; The terminal station is On The Moon &nbsp;&nbsp;&nbsp;&nbsp; Next stop is ${element.innerHTML} &nbsp;&nbsp;&nbsp;&nbsp; please get ready to Exit, from the left side.`
+        console.log(element.id)
     })
-    // mini1.addEventListener("click", () => {
-    //     console.log(scrollingText.innerHTML)
-    //     scrollingText.innerHTML = `Welcome to the metro line Iris &nbsp;&nbsp;&nbsp;&nbsp; The terminal station is On The Moon &nbsp;&nbsp;&nbsp;&nbsp; Next stop is Mini Project #6 &nbsp;&nbsp;&nbsp;&nbsp; please get ready to Exit, from the left side.`
-    // })
+})
+
+window.onload = function() {
+    door1.style.animationName = "openDoor";
+    door1.style.animationDuration = "5s";
+    door1.style.animationIterationCount = "1"
+    door2.style.animationName = "openDoor2";
+    door2.style.animationDuration = "5s";
+    door2.style.animationIterationCount = "1"
+
+    setTimeout(() => {
+        metroBackground.style.animationName = 'backgroundEnlarge';
+        metroBackground.style.animationDuration = "3s";
+        metroBackground.style.animationIterationCount = "1"
+        door1.style.display = 'none';
+        door2.style.display = 'none';
+
+    }, 4000);
+
+    setTimeout(() => {
+        metroBackground.style.display = "none"
+
+    }, 7000);
+
+}
+
+window.addEventListener('resize', () => {
+    door1.style.height = `${metroBackground.height * 0.84}px`
+    door2.style.height = `${metroBackground.height * 0.84}px`
+    door1.style.top = `${metroBackground.height * 0.17}px`
+    door2.style.top = `${metroBackground.height * 0.17}px`
+
+})
